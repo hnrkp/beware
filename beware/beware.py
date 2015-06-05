7d6efe3e5831982b7aad28044d7f954f06641d54
Henrik Persson <root@fulhack.info>
2015-06-06 00:00:03 +0200

    Use deferToThread, so that CGI calls won't block main I/O
    Fixes #9.

--- beware/beware.py
+++ beware/beware.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from twisted.internet import reactor
+from twisted.internet import reactor, defer, threads
 from twisted.web import static, server, html
 from twisted.web.resource import Resource
 from datetime import datetime, timedelta
@@ -56,9 +56,15 @@ def sessionExpired(request):
     return toUrl("?error=Session%20expired,%20please%20login%20again.", request)
 
 def toUrl(url, request):
-    getRoot(request)
     return redirectTo(getRoot(request) + url, request)
     
+def defaultErrback(failure, request):
+    failure.printTraceback()
+    
+    # We usually assume logged out on failure
+    request.write(relogin(request))
+    request.finish()
+    return None
 
 class Index(Resource):
     def getChild(self, name, request):
@@ -77,6 +83,20 @@ class Index(Resource):
         return template.render(siteRenderArgs, error=error).encode("utf-8")
 
 class Login(Resource):
+    def async_finish(self, sessionId, request):
+        user = request.getSession().user
+        
+        if sessionId < 0:
+            print("Login failed for user " + user + " from " + str(request.getClientIP()))
+            return toUrl("?error=Login%20failed", request)
+        
+        print("Login successful for user " + user + " from " + str(request.getClientIP()))
+        
+        request.getSession().bewator_session = int(sessionId)
+        
+        toUrl("objects", request)
+        request.finish()
+
     def render_POST(self, request):
         if not "user" in request.args or not "password" in request.args:
             return badRequest(request, "Please don't call me without the proper parameters!")
@@ -85,39 +105,68 @@ class Login(Resource):
         password = request.args["password"][0]
         
         session = request.getSession()
+        session.user = user
         session.bcgi = BewatorCgi(URL)
-        sessionId = session.bcgi.login(html.escape(user), password)
+
+        d = threads.deferToThread(session.bcgi.login, html.escape(user), password)
+        d.addCallback(self.async_finish, request)
+        d.addErrback(defaultErrback, request)
         
-        if sessionId < 0:
-            print("Login failed for user " + user + " from " + str(request.getClientIP()))
-            return toUrl("?error=Login%20failed", request)
-        
-        print("Login successful for user " + user + " from " + str(request.getClientIP()))
-        
-        session.bewator_session = int(sessionId)
-        
-        return toUrl("objects", request)
+        return server.NOT_DONE_YET
 
 class ListObjects(Resource):
+    def async_errback(self, failure, request):
+        failure.printTraceback()
+        
+        # We usually assume logged out on failure
+        sessionExpired(request)
+        request.finish()
+        return None
+    
+    def async_finish(self, objects, request):
+        template = env.get_template("objects.html")
+        request.write(template.render(siteRenderArgs, objects=objects).encode("utf-8"))
+        request.finish()
+    
     def render_GET(self, request):
         session = request.getSession()
         if not hasattr(session, "bewator_session"):
             return sessionExpired(request)
         
-        try:
-            objects = session.bcgi.getBookingObjects(session.bewator_session)
-        except Exception:
-            # Assume logged out if exception is thrown, log the exception and relogin user.
-            print('-'*60)
-            traceback.print_exc(file=sys.stdout)
-            print('-'*60)
-
-            return relogin(request)
+        d = threads.deferToThread(session.bcgi.getBookingObjects, session.bewator_session)
+        d.addCallback(self.async_finish, request)
+        d.addErrback(self.async_errback, request)
         
-        template = env.get_template("objects.html")
-        return template.render(siteRenderArgs, objects=objects).encode("utf-8")
-
+        return server.NOT_DONE_YET
+    
 class ListReservations(Resource):
+    def async_finish(self, tup, request, obj, nowTime, myTime):
+        (res, reservations) = tup
+        
+        if res == 49:
+            request.write(relogin(request))
+            request.finish()
+            return
+        
+        if res != 48:
+            print("ERROR: Res was something not OK, throw relogin (" + str(res) +")")
+            request.write(relogin(request))
+            request.finish()
+            return
+        
+        # Mark reservations in the past as "3".
+        for r in reservations:
+            if r.endTs < nowTime:
+                r.state = 3
+        
+        nextTs = myTime + 86400 * 7
+        prevTs = myTime - 86400 * 7
+        
+        request.write(env.get_template("reservations.html").render(siteRenderArgs, object=obj, curTs=myTime, prevTs=prevTs, nextTs=nextTs,
+                                                            reservations=reservations).encode("utf-8"))
+        request.finish()
+        
+    
     def render_GET(self, request):
         session = request.getSession()
         if not hasattr(session, "bewator_session"):
@@ -146,35 +195,33 @@ class ListReservations(Resource):
         else:
             myTime = startTime
         
-        try:
-            (res, reservations) = session.bcgi.getReservations(session.bewator_session, obj, myTime)
-        except Exception:
-            # Assume logged out if exception is thrown, log the exception and relogin user.
-            print('-'*60)
-            traceback.print_exc(file=sys.stdout)
-            print('-'*60)
-            
-            return relogin(request)
-        
-        if res == 49:
-            return relogin(request)
-        
-        if res != 48:
-            print("ERROR: Res was something not OK, throw relogin (" + str(res) +")")
-            return relogin(request)
-        
-        # Mark reservations in the past as "3".
-        for r in reservations:
-            if r.endTs < nowTime:
-                r.state = 3
-        
-        nextTs = myTime + 86400 * 7
-        prevTs = myTime - 86400 * 7
-        
-        return env.get_template("reservations.html").render(siteRenderArgs, object=obj, curTs=myTime, prevTs=prevTs, nextTs=nextTs,
-                                                            reservations=reservations).encode("utf-8")
+        d = threads.deferToThread(session.bcgi.getReservations, session.bewator_session, obj, myTime)
+        d.addCallback(self.async_finish, request, obj, nowTime, myTime)
+        d.addErrback(defaultErrback, request)
+
+        return server.NOT_DONE_YET
 
 class MakeReservation(Resource):
+    def async_finish(self, res, request):
+        if (res == 48):
+            request.write("All ok!")
+        elif (res == 49):
+            request.write(relogin(request))
+        elif (res == 50):
+            request.write(bewatorRequestError(request, "Someone has reserved this time already. Sorry."))
+        elif (res == 51):
+            request.write(bewatorRequestError(request, "Max number of reservations reached for your group."))
+        elif (res == 52):
+            request.write(bewatorRequestError(request, "Time interval no longer available."))
+        elif (res == 53):
+            request.write(bewatorRequestError(request, "Max number of reservations reached for this period."))
+        elif (res == 54):
+            request.write(bewatorRequestError(request, "Booking object is in service state, please try again later."))
+        else:
+            request.write(bewatorRequestError(request, "Unknown error: %d" % (res, )))
+        
+        request.finish()
+    
     def render_GET(self, request):
         session = request.getSession()
         if not hasattr(session, "bewator_session"):
@@ -198,32 +245,23 @@ class MakeReservation(Resource):
         start = int(start)
         end = int(end)
         
-        res = session.bcgi.makeReservation(session.bewator_session, obj, start, end)
-
-        if (res == 48):
-            return "All ok!"
-
-        if (res == 49):
-            return relogin(request)
+        d = threads.deferToThread(session.bcgi.makeReservation, session.bewator_session, obj, start, end)
+        d.addCallback(self.async_finish, request)
+        d.addErrback(defaultErrback, request)
         
-        if (res == 50):
-            return bewatorRequestError(request, "Someone has reserved this time already. Sorry.")
-        
-        if (res == 51):
-            return bewatorRequestError(request, "Max number of reservations reached for your group.")
-        
-        if (res == 52):
-            return bewatorRequestError(request, "Time interval no longer available.")
-        
-        if (res == 53):
-            return bewatorRequestError(request, "Max number of reservations reached for this period.")
-        
-        if (res == 54):
-            return bewatorRequestError(request, "Booking object is in service state, please try again later.")
-        
-        return bewatorRequestError(request, "Unknown error: %d" % (res, ))
+        return server.NOT_DONE_YET
 
 class CancelReservation(Resource):
+    def async_finish(self, res, request):
+        if (res == 48):
+            request.write("All ok!")
+        else:
+            # Else something else (probably tried to cancel someone else's reservation.
+            # Not likely to happen, but..
+            request.write(bewatorRequestError(request, "You can only cancel your own reservations."))
+        
+        request.finish()
+
     def render_GET(self, request):
         session = request.getSession()
         if not hasattr(session, "bewator_session"):
@@ -242,15 +280,11 @@ class CancelReservation(Resource):
         obj = int(obj)
         start = int(start)
         
-        res = session.bcgi.cancelReservation(session.bewator_session, obj, start)
+        d = threads.deferToThread(session.bcgi.cancelReservation, session.bewator_session, obj, start)
+        d.addCallback(self.async_finish, request)
+        d.addErrback(defaultErrback, request)
         
-        if (res == 48):
-            return "All ok!"
-        
-        # Else something else (probably tried to cancel someone else's reservation.
-        # Not likely to happen, but..
-        return bewatorRequestError(request, "You can only cancel your own reservations.")
-
+        return server.NOT_DONE_YET
 
 if __name__ == "__main__":
     root = Index()
